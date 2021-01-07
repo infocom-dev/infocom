2df81e6c700421402a4b626f49531532c830ca65
sharg <jenya.sharunov@gmail.com>
2021-01-07 15:51:32 +0300

    add volumes and env file

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+#URL /getAllChatByUser/
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.decorators import api_view, authentication_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST
+
+
+@api_view(['GET'])
+def api_gretting(request):
+    return Response({'chats': "Hello"})
--- docker/backend_setup.sh
+++ docker/backend_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-
+sleep 5;
 yes | python manage.py makemigrations # This is a development thing so I don't have to run it manually, but will be removed in production
 yes | python manage.py migrate
 yes | python manage.py loaddata categories
-
+sleep 5;
 python manage.py runserver 0.0.0.0:8000
