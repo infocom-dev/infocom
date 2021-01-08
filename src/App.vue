0b6aa8e7e8cca4b1f6a6e680028fa2f6eec9d814
TatyanaKonina <tdkonina@edu.hse.ru>
2021-01-08 20:08:08 +0300

    try to solve api problem

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-  <v-question/>
+    <p>helRRGGGGGRRRfffflo</p>
+  <!-- <v-question/> -->
   <!-- <label id="fruitLabel">Choose a fruit: </label>
     <combobox :items="[ 'Apple', 'Banana', 'Orange', 'Mango', 'Pear', 'Peach', 'Grape', 'Tangerine', 'Pineapple']" aria-labelled-by="fruitLabel" />
 
@@ -13,12 +14,12 @@
 </template>
 
 <script>
-import vQuestion from './components/v-question.vue'
+// import vQuestion from './components/v-question.vue'
 // import combobox from './components/combobox.vue'
 export default {
   name: 'App',
   components: {
-    vQuestion
+    // vQuestion
     // combobox
   },
   data(){
