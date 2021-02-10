e29ca2d9c8b04470e2fa6beb1b5e7e9692441acf
wityat <ttrttr449@gmail.com>
2021-02-10 13:04:47 +0300

    add secret and nice names of models

--- backend/api/models.py
+++ backend/api/models.py
@@ -13,7 +13,13 @@ class Question(models.Model):
     text = models.TextField("Текст вопроса")
     type = models.ForeignKey(QuestionType, on_delete=models.CASCADE, related_name='question')
 
+    def __str__(self):
+        return f"{self.text}"
+
 
 class Answer(models.Model):
     value = models.TextField("Значение вопроса")
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+
+    def __str__(self):
+        return f"{self.value}"
