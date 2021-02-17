1382c244146d9acf47103a7fc35e7cd2c38656a0
TatyanaKonina <tdkonina@edu.hse.ru>
2021-02-17 23:00:00 +0300

    add resizable slider

--- src/components/v-home-page.vue
+++ src/components/v-home-page.vue
@@ -1,178 +1,165 @@
 <template>
-  <div class="v-home-page"> 
-    <v-navigation-wrapper></v-navigation-wrapper>  
-       <div class="carousel">
-        <slider autoplay="false" animation="fade"  width="1200px" height="500px"  >
-  <slider-item 
-   >
-   <div class="carousel-content">
-     <h1>INFO<span>COM</span></h1>
-     <h3>WEB DEVELOPMENT SOLUTIONS</h3>
-     <p></p>
-   </div>
-  </slider-item>
-  <slider-item >
-    <div class="carousel-content-second">
-      <h1>Need a <span>chat bot</span>?</h1>
-      <div class="button">
-        <router-link :to="{name : 'anketa'}">
-            <button>Click here</button>
-        </router-link>
+  <div class="v-home-page">
+    <section class="home">
+      <v-navigation-wrapper></v-navigation-wrapper>
+      <div class="slider-container">
+        <div class="slider">
+          <div class="slide">
+            <div class="slide-text">
+              <h1>Need a <span>chat bot</span>?</h1>
+
+              <router-link :to="{ name: 'anketa' }">
+                <button>Click here</button>
+              </router-link>
+
+              <p>Fill out the form and find out the price!</p>
+            </div>
+          </div>
+          <div class="slide">
+            <div class="slide-text">
+              <h1>INFO<span>COM</span></h1>
+              <h3>WEB DEVELOPMENT SOLUTIONS</h3>
+              <p></p>
+            </div>
+          </div>
+          <div class="slide">
+            <div class="slide-text"></div>
+          </div>
+        </div>
       </div>
-      <p>Fill out the form and find out the price!</p>
-   </div>
-  </slider-item>
-</slider>
-      </div>  
-       <div class="slider-container">
-  <div class="slider">
-    <div class="slide">
-      <div class="slide-text">Slide 1 text</div>
-    </div>
-    <div class="slide">
-      <div class="slide-text">Slide 2 text</div>
-    </div>
-    <div class="slide">
-      <div class="slide-text">Slide 3 text</div>
-    </div>
-  </div>
-</div> 
-<section class="service">
+    </section>
+    <section class="service">
   <h2>OUR <span>SERVICeS</span></h2>
 </section>
   </div>
 </template>
 
 <script>
-
-import { Slider, SliderItem } from 'vue-easy-slider'
-import vNavigationWrapper from './v-navigation-wrapper'
-import VNavigationWrapper from './v-navigation-wrapper.vue'
+// import { Slider, SliderItem } from 'vue-easy-slider'
+import vNavigationWrapper from "./v-navigation-wrapper";
+import VNavigationWrapper from "./v-navigation-wrapper.vue";
 
 export default {
-  name: 'v-home-page',
+  name: "v-home-page",
   components: {
-    Slider,
-    SliderItem,
+    // Slider,
+    // SliderItem,
     vNavigationWrapper,
-    VNavigationWrapper
+    VNavigationWrapper,
   },
-  data(){
-    return {  
-    }
+  data() {
+    return {};
   },
-  methods:{
-  },
-
-}
+  methods: {},
+};
 </script>
 
 <style lang="scss" >
-
-
-@import '../assets/styles/base.scss';
-@import '../assets/styles/button.scss';
-@import '../assets/styles/fonts.scss';
-@import '../assets/styles/navigation.scss';
-.v-home-page{
+@import "../assets/styles/base.scss";
+@import "../assets/styles/button.scss";
+@import "../assets/styles/fonts.scss";
+@import "../assets/styles/navigation.scss";
+.v-home-page {
   margin: 0 auto;
-  font-family: 'Lato', sans-serif;
-  background: url('../assets/images/10.jpg') no-repeat center center;
+  font-family: "Lato", sans-serif;
+  // background: url('../assets/images/10.jpg') no-repeat center center;
+  // background-size: cover;
+  // position: relative;
+  // justify-content: center;
+  // flex-direction: column;
+  // // display: flex;
+  // height: 700px;
+}
+.home {
+  background: url("../assets/images/10.jpg") no-repeat center center;
   background-size: cover;
   position: relative;
-  justify-content: center;
+  // justify-content: center;
   flex-direction: column;
-  //display: flex;
+  // display: flex;
   height: 700px;
 
-}
-// .slider{
-//   margin:0 auto; 
-// }
-// .slider-item{
-//   display: flex;
-//   justify-content: center;
-//   margin:60px auto;
-//   text-align: center;
-//   background: $bg;
-//   // background: rgba(#262626,0.3);
-//   padding-top: 20px;
-//   h1{
-//     font-size: 100px;
-//     letter-spacing:5px
-//   }
-//   span{
-//     font-size: 80px;
-//   }
-
-// }
-// .button{
-//   button{
-//     width: 200px;
-//   }
-// }
-
-*{
-  box-sizing: border-box;
+  // display: flex;
+  // align-items: center;
+  // justify-content: center;
 }
 
-body{
-  margin:0;
-}
-
-.slider-container{
+.slider-container {
+  // margin: 0 auto;
+  // display: flex;
+  // align-items: center;
+  // justify-content: center;
+  // display: flex;
+  // align-self: center;
+  // text-align: center;
   width: 100vw;
   height: 50vh;
-  overflow: hidden;
+
+  top: 25%;
+  //overflow: hidden;
   position: relative;
-  .slider{
+  .slider {
     width: 300%;
     height: 100%;
     position: absolute;
-    top:0;
-    left:0;
+    //top:0;
+    //left:0;
     transform: translateX(0);
     animation: slide1 15s infinite;
-    
-    .slide{
+
+    button {
+      width: 200px;
+    }
+
+    .slide {
       width: 33.333333333%;
       height: 100%;
       float: left;
-      background: #eee;
+      // background: $bg;
       position: relative;
-      
-      &:nth-child(1){
-        background: url(https://images.unsplash.com/photo-1433838552652-f9a46b332c40?q=80&fm=jpg&s=97cb7cd5e219e932365f3bcff3950eda) no-repeat center;
-            background-size: cover;
-      }
-      
-      &:nth-child(2){
-        background: url(https://images.unsplash.com/photo-1432836431433-925d3cc0a5cd?q=80&fm=jpg&s=97a1e6c0e5adecf5dc6d53e17d6bc581) no-repeat center;
-            background-size: cover;
-      }
-      &:nth-child(3){
-        background: url(https://images.unsplash.com/photo-1428278953961-a8bc45e05f72?q=80&fm=jpg&s=24f29a71c4de9b922e7ee2cb5ee46f4b) no-repeat center;
-            background-size: cover;
-      }
-      
-      
-      img{
+      display: flex;
+      justify-content: center;
+      // margin:60px auto;
+      // &:nth-child(1){
+      //   background: url(https://images.unsplash.com/photo-1433838552652-f9a46b332c40?q=80&fm=jpg&s=97cb7cd5e219e932365f3bcff3950eda) no-repeat center;
+      //       background-size: cover;
+      // }
+
+      // &:nth-child(2){
+      //   background: url(https://images.unsplash.com/photo-1432836431433-925d3cc0a5cd?q=80&fm=jpg&s=97a1e6c0e5adecf5dc6d53e17d6bc581) no-repeat center;
+      //       background-size: cover;
+      // }
+      // &:nth-child(3){
+      //   background: url(https://images.unsplash.com/photo-1428278953961-a8bc45e05f72?q=80&fm=jpg&s=24f29a71c4de9b922e7ee2cb5ee46f4b) no-repeat center;
+      //       background-size: cover;
+      // }
+
+      img {
         display: block;
         max-width: 100%;
         height: auto;
       }
-      
-      .slide-text{
+
+      .slide-text {
+        text-align: center;
         display: inline-block;
         position: absolute;
-        left: 20px;
+        align-self: center;
+        justify-content: center;
         top: 50%;
         font-family: arial, Helvetica;
-        text-transform: uppercase;
+        // text-transform: uppercase;
         font-size: 2.5vw;
-        background: #fff;
+
         padding: 5px;
         transform: translateY(-50%);
+        h1 {
+          font-size: 70px;
+          letter-spacing: 5px;
+        }
+        span {
+          font-size: 50px;
+        }
         //opacity:0;
         //animation-delay: 0.2s;
       }
@@ -180,36 +167,33 @@ body{
   }
 }
 
-
-@media all and (min-width: 1400px){
-  .slider-container{
-    min-height:80vh; 
+@media all and (min-width: 1400px) {
+  .slider-container {
+    min-height: 80vh;
   }
-  
 }
 
-@keyframes slide1{
- 27%{
-    transform: translateX(0);
-  }  
- 33%{
-    transform: translateX(-33.3333333333%);
-  }  
-  60%{
-    transform: translateX(-33.3333333333%);
-  }
-  66%{
-    transform: translateX(-66.6666666666%);
-  }
-  
-  94%{
-    transform: translateX(-66.6666666666%);
-  }
-  
-  100%{
+@keyframes slide1 {
+  27% {
     transform: translateX(0);
   }
- 
+  33% {
+    transform: translateX(-33.3333333333%);
+  }
+  60% {
+    transform: translateX(-33.3333333333%);
+  }
+  66% {
+    transform: translateX(-66.6666666666%);
+  }
+
+  94% {
+    transform: translateX(-66.6666666666%);
+  }
+
+  100% {
+    transform: translateX(0);
+  }
 }
 
 .service{
@@ -217,6 +201,4 @@ body{
 	background-color: #000;
 	color: #fff;
 }
-
-
 </style>
--- src/components/v-navigation-wrapper.vue
+++ src/components/v-navigation-wrapper.vue
@@ -1,74 +1,59 @@
 
 <template>
-<div class="v-navigation-wrapper">
+  <div class="v-navigation-wrapper">
     <div class="menu-wrap">
-       <div class = "logo">
-              <h1>INFO<span>com</span></h1>
-          </div>
-          <!-- <input type="checkbox" id="checkbox"> -->
-       <nav v-if="state">
-           <ul>
-               <li><a href="#">Home</a></li>
-               <li><a href="#">About</a></li>
-               <li><a href="#">Services</a></li>
-               <li><a href="#">Contact</a></li>
-               <!-- <button @click="myFunction"></button> -->
-           </ul>
-       </nav>
-       <!-- <button @click="myFunction"></button> -->
-       
-       <button @click="myFunction">
-           <i  class="fa fa-bars menu-icon"  ></i>
-       </button>
-
-   </div>
-    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <div class = "navigation">
-          <div class = "logo">
-              <h1>INFO<span>com</span></h1>
-          </div>
-          <input type="checkbox" id="checkbox">
-           <div class="menu">
-                <ul class="nav">
-                   <li><a class="link">HOME</a></li>
-                   <li><a class="link">SERVICE</a></li>
-                   <li><a class="link">TEAM</a></li>
-                   <li><a class="link">CONTACT</a></li>
-                </ul>
-            </div>
-            
-            <i class="fa fa-bars menu-icon"><input type="checkbox" id="checkbox"></i>
+      <div class="logo">
+        <h1>INFO<span>com</span></h1>
       </div>
-       -->
-    
-</div>
-    
+
+      <nav v-if="!(state && small)">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+      <div class="button">
+        <button @click="myFunction">
+          <i class="fa fa-bars menu-icon"></i>
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script >
-
 export default {
-name:"v-navigation-wrapper",
-data(){
-    return{
-        state:true
-    }
-},
-methods:{
+  name: "v-navigation-wrapper",
+  data() {
+    return {
+      state: true,
+      small: false,
+    };
+  },
+  created() {
+    window.addEventListener("resize", this.onResize);
+    this.onResize();
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.onResize);
+  },
+  methods: {
     myFunction() {
-    this.state = !this.state;
-}
-}
-}
-
-
+      this.state = !this.state;
+    },
+    onResize() {
+      this.small = window.innerWidth <= 768;
+    },
+  },
+};
 </script>
 <style lang="scss" >
 * {
-   margin: 0;
-   padding: 0;
-   box-sizing: border-box;
-   font-family: sans-serif;
-   
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: sans-serif;
 }
 </style>
