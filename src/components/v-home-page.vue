44ecea546c23f5480cb61f5cc7055ce6147d34d6
TatyanaKonina <tdkonina@edu.hse.ru>
2021-02-20 13:56:37 +0300

    fix style

--- src/components/v-home-page.vue
+++ src/components/v-home-page.vue
@@ -5,7 +5,7 @@
         <v-navigation-wrapper></v-navigation-wrapper>
         <div class="container">
           <h3>Our Clients Are Our First Priority</h3>
-          <h1>WELCOME TO BINO</h1>
+          <h1>WELCOME TO INFOCOM</h1>
           <div class="separator"></div>
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -18,31 +18,6 @@
           </div>
         </div>
       </div>
-      <!-- <div class="slider-container">
-        <div class="slider">
-          <div class="slide">
-            <div class="slide-text">
-              <h1>Need a <span>chat bot</span>?</h1>
-
-              <router-link :to="{ name: 'anketa' }">
-                <button>Click here</button>
-              </router-link>
-
-              <p>Fill out the form and find out the price!</p>
-            </div>
-          </div>
-          <div class="slide">
-            <div class="slide-text">
-              <h1>INFO<span>COM</span></h1>
-              <h3>WEB DEVELOPMENT SOLUTIONS</h3>
-              <p></p>
-            </div>
-          </div>
-          <div class="slide">
-            <div class="slide-text"></div>
-          </div>
-        </div>
-      </div> -->
     </section>
     <section class="service">
       <div class="header-section">
@@ -50,8 +25,16 @@
       </div>
       <div class="container-adapt">
         <div class="service-item">
+          <figure class="image is-fullwidth">
+            <img class="animated-img" src="../assets/images/home.svg" />
+          </figure>
+        </div>
+        <div class="service-item">
           <div class="content-container">
             <div class="row">
+              <div class="icon">
+                <i class="fa fa-calculator chart-icon"> </i>
+              </div>
               <div class="text">
                 <h4>Calculate</h4>
                 <p>
@@ -60,13 +43,13 @@
                   standard dummy text ever.
                 </p>
               </div>
-              <div class="icon">
-                <i class="fa fa-calculator chart-icon"> </i>
-              </div>
             </div>
           </div>
           <div class="content-container">
             <div class="row">
+              <div class="icon">
+                <i class="fa fa-laptop laptop-icon"></i>
+              </div>
               <div class="text">
                 <h4>web development</h4>
                 <p>
@@ -75,13 +58,13 @@
                   standard dummy text ever.
                 </p>
               </div>
-              <div class="icon">
-                <i class="fa fa-laptop laptop-icon"></i>
-              </div>
             </div>
           </div>
           <div class="content-container">
             <div class="row">
+              <div class="icon">
+                <i class="fa fa-comments chat-icon"></i>
+              </div>
               <div class="text">
                 <h4>chat-bot</h4>
                 <p>
@@ -90,40 +73,9 @@
                   standard dummy text ever.
                 </p>
               </div>
-              <div class="icon">
-                <i class="fa fa-comments chat-icon"></i>
-              </div>
             </div>
           </div>
         </div>
-        <div class="service-item">
-          <figure class="image is-fullwidth">
-            <img class="animated-img" src="../assets/images/home.svg" />
-          </figure>
-        </div>
-        <!-- <div class="service-item">
-          <i class="fa fa-laptop laptop-icon"></i>
-          <hr />
-          <h4>web development</h4>
-          <p>lalaaa lalalffffal laalalal alalaalal lalall</p>
-          <button>more</button>
-        </div>
-        <div class="service-item">
-          <i class="fa fa-comments chat-icon" aria-hidden="true"></i>
-          <hr />
-          <h4>chat-bot</h4>
-          <p>Telegram and vk bot development vvfff</p>
-          <button>more</button>
-        </div> -->
-
-        <!-- <div class="service-item">
-          <i class="fa fa-database data-icon" aria-hidden="true"></i>
-          <h4>lalala</h4>
-          <p>
-             Какой то текстю.lalal all lal alalla fflflflf
-          </p>
-          <button>more</button>
-        </div> -->
       </div>
     </section>
     <section class="promo-banner">
@@ -207,30 +159,28 @@ export default {
   height: 770px;
   font-size: 1rem;
 }
-.home-skew-border{
-    // position: relative;
-
+.home-skew-border {
+  // position: relative;
 }
-.home-skew-border:after{
-    position: absolute;
-    left:0;
-    bottom:0;
-    content: "";
-    width: 0;
-    height: 0;
-    border-bottom: 86px solid #fff;
-    border-right: 680px solid transparent;
-
+.home-skew-border:after {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  content: "";
+  width: 0;
+  height: 0;
+  border-bottom: 86px solid $wht;
+  border-right: 680px solid transparent;
 }
-.home-skew-border:before{
-    position: absolute;
-    right:0;
-    bottom:0;
-    content: "";
-    width: 0;
-    height: 0;
-    border-bottom: 86px solid #fff;
-    border-left: 680px solid transparent;
+.home-skew-border:before {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  content: "";
+  width: 0;
+  height: 0;
+  border-bottom: 86px solid $wht;
+  border-left: 680px solid transparent;
 }
 
 .container {
@@ -259,7 +209,7 @@ export default {
     width: 10px;
     height: 10px;
     margin: 20px auto;
-    background: $rs;
+    background: $ylw;
     position: relative;
     display: inline-block;
     border-radius: 50%;
@@ -274,7 +224,7 @@ export default {
     bottom: 4px;
     height: 1px;
     width: 80px;
-    background: #eee;
+    background: $wht;
     content: "";
     display: inline-block;
     margin-right: 20px;
@@ -289,7 +239,7 @@ export default {
     bottom: 4px;
     height: 1px;
     width: 80px;
-    background: #eee;
+    background: $wht;
     content: "";
     display: inline-block;
     margin-left: 20px;
@@ -307,167 +257,7 @@ export default {
   html {
     font-size: 70%;
   }
-  // .service {
-  //   padding-right: 150px;
-  //   padding-left: 150px;
-  // }
 }
-// .slider-container {
-//   width: 100vw;
-//   height: 50vh;
-//   top: 25%;
-//   position: relative;
-//   .slider {
-//     width: 300%;
-//     height: 100%;
-//     position: absolute;
-//     transform: translateX(0);
-//     animation: slide1 15s infinite;
-//     button {
-//       width: 200px;
-//     }
-//     .slide {
-//       width: 33.333333333%;
-//       height: 100%;
-//       float: left;
-//       position: relative;
-//       display: flex;
-//       justify-content: center;
-//       // margin:60px auto;
-//       // &:nth-child(1){
-//       //   background: url(https://images.unsplash.com/photo-1433838552652-f9a46b332c40?q=80&fm=jpg&s=97cb7cd5e219e932365f3bcff3950eda) no-repeat center;
-//       //       background-size: cover;
-//       // }
-//       // &:nth-child(2){
-//       //   background: url(https://images.unsplash.com/photo-1432836431433-925d3cc0a5cd?q=80&fm=jpg&s=97a1e6c0e5adecf5dc6d53e17d6bc581) no-repeat center;
-//       //       background-size: cover;
-//       // }
-//       // &:nth-child(3){
-//       //   background: url(https://images.unsplash.com/photo-1428278953961-a8bc45e05f72?q=80&fm=jpg&s=24f29a71c4de9b922e7ee2cb5ee46f4b) no-repeat center;
-//       //       background-size: cover;
-//       // }
-//       img {
-//         display: block;
-//         max-width: 100%;
-//         height: auto;
-//       }
-//       .slide-text {
-//         text-align: center;
-//         display: inline-block;
-//         position: absolute;
-//         align-self: center;
-//         justify-content: center;
-//         top: 50%;
-//         font-family: arial, Helvetica;
-//         // text-transform: uppercase;
-//         font-size: 2.5vw;
-//         padding: 5px;
-//         transform: translateY(-50%);
-//         // h1 {
-//         //   font-size: 70px;
-//         //   letter-spacing: 5px;
-//         // }
-//         // span {
-//         //   font-size: 50px;
-//         // }
-//         //opacity:0;
-//         //animation-delay: 0.2s;
-//       }
-//     }
-//   }
-// }
-@media all and (min-width: 200px) {
-  .slide-text {
-    h1,
-    h3 {
-      font-size: 25px;
-      letter-spacing: 1px;
-    }
-    span {
-      font-size: 20px;
-    }
-    p {
-      font-size: 20px;
-    }
-    button {
-      padding: 6px 10px 0;
-      border-radius: 0;
-      font-weight: 700;
-      font-size: 18px;
-    }
-  }
-}
-
-@media all and (min-width: 400px) {
-  .slide-text {
-    h1,
-    h3 {
-      font-size: 50px;
-      letter-spacing: 1px;
-    }
-    span {
-      font-size: 40px;
-    }
-    p {
-      font-size: 25px;
-    }
-    button {
-      padding: 6px 10px 0;
-      border-radius: 0;
-      font-weight: 700;
-      font-size: 25px;
-    }
-  }
-}
-@media all and (min-width: 1400px) {
-  .slider-container {
-    min-height: 80vh;
-    top: 10%;
-  }
-  .slide-text {
-    h1,
-    h3 {
-      font-size: 50px;
-      letter-spacing: 1px;
-    }
-    span {
-      font-size: 40px;
-    }
-    p {
-      font-size: 25px;
-    }
-    button {
-      padding: 6px 10px 0;
-      border-radius: 0;
-      font-weight: 700;
-      font-size: 25px;
-    }
-  }
-}
-
-@keyframes slide1 {
-  27% {
-    transform: translateX(0);
-  }
-  33% {
-    transform: translateX(-33.3333333333%);
-  }
-  60% {
-    transform: translateX(-33.3333333333%);
-  }
-  66% {
-    transform: translateX(-66.6666666666%);
-  }
-
-  94% {
-    transform: translateX(-66.6666666666%);
-  }
-
-  100% {
-    transform: translateX(0);
-  }
-}
-
 .service {
   position: relative;
   z-index: 1;
@@ -476,14 +266,9 @@ export default {
   background-size: auto;
   background-size: cover;
   min-height: 100%;
-  // background-color: url(../assets/images/10.jpg);
   color: $wht;
-  // height: 400px;
   margin: 0 auto;
   align-content: center;
-  // margin-bottom: 1px;
-  // padding-right: 50px;
-  // padding-left: 50px;
   margin-right: auto;
   margin-left: auto;
 }
@@ -509,7 +294,7 @@ export default {
   display: flex;
 }
 .text {
-  text-align: right;
+  text-align: left;
   width: 80%;
   float: left;
 }
@@ -528,29 +313,16 @@ export default {
   box-sizing: border-box; /* Изменения алгоритма расчета ширины и высоты элемента.*/
   -moz-box-sizing: border-box; /* Для Firefox */
   -webkit-box-sizing: border-box; /* Для Safari, Chrome, iOS иAndroid */
-  // box-shadow: 2px 2px 4px 0 #ccc; /* Внешняя тень плиток */
-  // border: 2px solid $red;
   height: 100%;
-  // margin-top: 50px;
   transition: all 0.6s;
-  // text-align: right;
   background-color: $wht;
   p {
     line-height: 24px;
-    // padding: 10px;
     color: $blu;
     font-size: 15px;
     margin: 0 0 10px;
   }
   h4 {
-    // .service {
-    //   padding-left: 150px;
-    //   padding-right: 150px;
-    // }
-    // .skills {
-    //   padding-right: 150px;
-    //   padding-left: 150px;
-    // }
     font-size: 18px;
     color: $rs;
     padding-bottom: 10px;
@@ -560,19 +332,19 @@ export default {
 }
 img,
 iframe {
-  width: 90%;
+  width: 100%;
   height: auto;
   display: block;
   text-align: center;
+  align-self: center;
 }
 .laptop-icon,
 .data-icon,
 .chart-icon,
 .chat-icon {
   font-size: 70px;
-  color: $rs;
+  color: $blu;
   line-height: 50px;
-
   padding: 10px;
 }
 .animated-img {
