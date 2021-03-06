1cf1cce429d27d990ff4b2aa6367a1c051c62146
wityat <wityat2013@yandex.ru>
2021-03-06 16:57:34 +0300

    fix Models lol

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -1,37 +1,37 @@
-from rest_framework import serializers
-
-from backend.api.models import QuestionType, Answer, Question
-from rest_framework.fields import IntegerField
-
-# from django.contrib.auth.models import User
-
-
-# class StringArrayField(IntegerField):
-#     """
-#     String representation of an array field.
-#     """
+# from rest_framework import serializers
 #
-#     def to_representation(self, value):
-#         return str(value)
-
-
-class AnswerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Answer
-        exclude = ("question",)
-
-
-class QuestionTypeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = QuestionType
-        exclude = ("question",)
-
-
-class QuestionSerializer(serializers.ModelSerializer):
-    type = serializers.StringRelatedField()
-    answers = AnswerSerializer(many=True)
-    class Meta:
-        model = Question
-        fields=('id','text','type','answers')
-
-
+# from backend.api.models import QuestionType, Answer, Question
+# from rest_framework.fields import IntegerField
+#
+# # from django.contrib.auth.models import User
+#
+#
+# # class StringArrayField(IntegerField):
+# #     """
+# #     String representation of an array field.
+# #     """
+# #
+# #     def to_representation(self, value):
+# #         return str(value)
+#
+#
+# class AnswerSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Answer
+#         exclude = ("question",)
+#
+#
+# class QuestionTypeSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = QuestionType
+#         exclude = ("question",)
+#
+#
+# class QuestionSerializer(serializers.ModelSerializer):
+#     type = serializers.StringRelatedField()
+#     answers = AnswerSerializer(many=True)
+#     class Meta:
+#         model = Question
+#         fields=('id','text','type','answers')
+#
+#
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from backend.api import views
 
 urlpatterns = ([
-    path("hello/", views.QuestionViewSet.as_view({'get': 'list'})),
+    # path("hello/", views.QuestionViewSet.as_view({'get': 'list'})),
 
     path('auth/', include('djoser.urls')),
     # path("auth/token/", obtain_auth_token,name='token'),
