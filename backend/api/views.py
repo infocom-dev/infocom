9497a405bdcce8aea133cd89159f7ff4036b6a5c
SharunovEvgeny <Ee95023495>
2021-03-09 18:44:59 +0300

    create new api method for Customer, Developer, Project

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,28 @@
-from rest_framework import viewsets,permissions
+from rest_framework import viewsets, permissions
 
-from backend.api.models import Question
-from backend.api.serializer import QuestionSerializer
-
-
+from backend.api.models import Question, Customer, Developer, Project
+from backend.api.serializer import QuestionSerializer, CustomerSerializer, DeveloperSerializer, ProjectSerializer
 
 
 class QuestionViewSet(viewsets.ModelViewSet):
     # permission_classes = [permissions.IsAuthenticated]
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    # permission_classes = [permissions.IsAuthenticated]
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class DeveloperViewSet(viewsets.ModelViewSet):
+    # permission_classes = [permissions.IsAuthenticated]
+    queryset = Developer.objects.all()
+    serializer_class = DeveloperSerializer
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    # permission_classes = [permissions.IsAuthenticated]
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
