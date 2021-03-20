9b8f3cb7e2b4742f13c87dfbafc1673b5d5234ca
TatyanaKonina <tdkonina@edu.hse.ru>
2021-03-20 08:08:50 +0300

    change homepage

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,29 +1,23 @@
 <template>
   <div class="NavBar">
-    <b-navbar slot="header" toggleable="lg" fixed="top" class="nav">
-      <b-navbar-brand class="mx-4">
-        <h1 class="text-uppercase font-weight-bolder">INFO<span>com</span></h1>
+    <b-navbar slot="header" toggleable="lg" fixed="top" class="nav justify-content-center">
+      <b-navbar-brand  class="mx-5" >
+        <h1 class="text-uppercase mx-5 font-weight-bolder my-auto">
+          INFO<span>com</span>
+        </h1>
       </b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav v-b-scrollspy:nav-scroller class="ml-auto">
+        <b-navbar-nav v-b-scrollspy:nav-scroller class="ml-auto pt-2">
           <div v-for="item in menu" :key="item">
             <b-nav-item size="lg" :href="item.href">
-              <h4 class="text-uppercase font-weight-bolder">{{ item.id }}</h4>
+              <h4 class="text-uppercase font-weight-bolder ">{{ item.id }}</h4>
             </b-nav-item>
           </div>
-          <b-nav-item-dropdown right>
-            <template #button-content>
-              <i class="fa fa-user user-icon" aria-hidden="true"></i>
-            </template>
-            
-              <b-dropdown-item href="#"><router-link to="login"> Sign In</router-link> </b-dropdown-item>
-            
-            <router-link to="logout">
-              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-            </router-link>
-          </b-nav-item-dropdown>
         </b-navbar-nav>
+        <div class="mr-5 ml-3 ">
+          <a href="#" class="btn">LOG IN</a>
+        </div>
       </b-collapse>
     </b-navbar>
   </div>
@@ -54,9 +48,11 @@ export default {
 </script>
 <style lang="scss">
 @import "../assets/styles/base.scss";
+@import "../assets/styles/button.scss";
 .nav {
   text-align: center;
-  background: $bg;
+  background: #A3CEF1;
+
   .user-icon,
   .navbar-toggler-icon,
   .navbar-light .nav-link .navbar-nav {
@@ -64,5 +60,10 @@ export default {
     -webkit-text-stroke-width: 1px;
     -webkit-text-stroke-color: black;
   }
+  box-shadow: 0 10px 22px rgba(0, 0, 0, 0.1)  ; 
+	-webkit-box-shadow: 0 10px 22px rgba(0, 0, 0, 0.1)  ; 
+	-moz-box-shadow: 0 10px 22px rgba(0, 0, 0, 0.1)  ; 
+  
 }
+
 </style>
--- src/components/v-home-page.vue
+++ src/components/v-home-page.vue
@@ -1,12 +1,48 @@
 <template>
   <div class="v-home-page">
-    <nav-bar ></nav-bar>
-    <div
-      id="nav-scroller"
-    >
-      <section id="home" class="home position-relative d-flex justify-content-center">
-        <div class="home-skew-border">
-          <div class="w-50 mx-auto text-center">
+    <nav-bar></nav-bar>
+    <div id="nav-scroller">
+      <section
+        id="home"
+        class="home position-relative d-flex justify-content-center"
+      >
+        <b-container fluid class="w-100">
+          <b-row>
+            <b-col lg sm>
+              <b-container fluid>
+                <b-row class="pt-5 mx-5">
+                  <div class="text-left">
+                    <p>Our Clients Are Our First Priority</p>
+                    <h1 class="mt-4 text-uppercase font-weight-bolder">
+                      WELCOME TO INFOCOM
+                    </h1>
+                    <div class="separator"></div>
+                    <p class="mt-4">
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry. Lorem Ipsum has been the industry's
+                      standard dummy text ever since the 1500s, when an unknown
+                      printer took a galley of type and scrambled it to make a
+                      type specimen book.
+                    </p>
+                    <!-- <br> -->
+                    <div class="w-25">
+                      <a href="#" class="btn">LEARN MORE</a>
+                    </div>
+                  </div>
+                </b-row>
+              </b-container>
+            </b-col>
+            <b-col lg>
+              <b-img
+                right
+                class="w-75"
+                :src="require('../assets/images/laptop2.gif')"
+              />
+            </b-col>
+          </b-row>
+        </b-container>
+        <!-- <div class="home-skew-border">
+          <div class="w-50  text-left">
             <p>Our Clients Are Our First Priority</p>
             <h1 class="text-uppercase font-weight-bolder">
               WELCOME TO INFOCOM
@@ -22,7 +58,7 @@
               >GET STARTED NOW</b-button
             ></router-link>
           </div>
-        </div>
+        </div> -->
       </section>
       <section id="services" class="service">
         <v-services-vue></v-services-vue>
@@ -49,7 +85,7 @@ import NavBar from "./NavBar.vue";
 import Footer from "./Footer.vue";
 import vServicesVue from "./v-services.vue";
 import VTeam from "./v-team.vue";
-import ContactUs from './contact-us.vue';
+import ContactUs from "./contact-us.vue";
 export default {
   name: "v-home-page",
   components: {
@@ -59,7 +95,6 @@ export default {
     VTeam,
     Footer,
     ContactUs,
-
   },
   data() {
     return {};
@@ -72,70 +107,71 @@ export default {
 @import "../assets/styles/button.scss";
 @import "../assets/styles/fonts.scss";
 .home {
-  background: url("../assets/images/09.jpg") no-repeat center center;
+  background: url("../assets/images/bg10.jpg");
+
   background-size: cover;
   // position: relative;
   flex-direction: column;
-  height: 770px;
+  height: 700px;
   font-size: 1rem;
 }
-.home-skew-border:after {
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  content: "";
-  width: 0;
-  height: 0;
-  border-bottom: 86px solid $wht;
-  border-right: 680px solid transparent;
-}
-.home-skew-border:before {
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  content: "";
-  width: 0;
-  height: 0;
-  border-bottom: 86px solid $wht;
-  border-left: 680px solid transparent;
-}
-.separator {
-  width: 10px;
-  height: 10px;
-  margin: 20px auto;
-  background: $ylw;
-  position: relative;
-  display: inline-block;
-  border-radius: 50%;
-}
-.separator::before {
-  -webkit-box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  box-sizing: border-box;
-  direction: rtl;
-  position: absolute;
-  right: 5%;
-  bottom: 4px;
-  height: 1px;
-  width: 80px;
-  background: $wht;
-  content: "";
-  display: inline-block;
-  margin-right: 20px;
-}
-.separator::after {
-  -webkit-box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  box-sizing: border-box;
-  direction: rtl;
-  position: absolute;
-  left: 5%;
-  bottom: 4px;
-  height: 1px;
-  width: 80px;
-  background: $wht;
-  content: "";
-  display: inline-block;
-  margin-left: 20px;
-}
+// .home-skew-border:after {
+//   position: absolute;
+//   left: 0;
+//   bottom: 0;
+//   content: "";
+//   width: 0;
+//   height: 0;
+//   border-bottom: 86px solid $wht;
+//   border-right: 680px solid transparent;
+// }
+// .home-skew-border:before {
+//   position: absolute;
+//   right: 0;
+//   bottom: 0;
+//   content: "";
+//   width: 0;
+//   height: 0;
+//   border-bottom: 86px solid $wht;
+//   border-left: 680px solid transparent;
+// }
+// .separator {
+//   width: 10px;
+//   height: 10px;
+//   margin: 20px auto;
+//   background: $ylw;
+//   position: relative;
+//   display: inline-block;
+//   border-radius: 50%;
+// }
+// .separator::before {
+//   -webkit-box-sizing: border-box;
+//   -moz-box-sizing: border-box;
+//   box-sizing: border-box;
+//   direction: rtl;
+//   position: absolute;
+//   right: 5%;
+//   bottom: 4px;
+//   height: 1px;
+//   width: 80px;
+//   background: $wht;
+//   content: "";
+//   display: inline-block;
+//   margin-right: 20px;
+// }
+// .separator::after {
+//   -webkit-box-sizing: border-box;
+//   -moz-box-sizing: border-box;
+//   box-sizing: border-box;
+//   direction: rtl;
+//   position: absolute;
+//   left: 5%;
+//   bottom: 4px;
+//   height: 1px;
+//   width: 80px;
+//   background: $wht;
+//   content: "";
+//   display: inline-block;
+//   margin-left: 20px;
+// }
 </style>
