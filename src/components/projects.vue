4df614537fb225c155d9ddb0bd89ea0f1def5068
TatyanaKonina <tdkonina@edu.hse.ru>
2021-04-27 18:22:02 +0300

    add anketa row

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -43,10 +43,44 @@
               </b-row>
             </b-col>
           </b-row>
-          <b-container fluid class="w-100 ml-0 overview">
-              
+          <b-container fluid class="w-100  overview">
+            <b-row align-v="center">
+              <div class="anketa-box w-100 m-5">
+                <b-row>
+                  <div class="col-3">
+                    <b-img
+                      style="max-width:300px"
+                      fluid
+                      center
+                      class=""
+                      :src="require('../assets/images/idea.png')"
+                    />
+                  </div>
+                  <div class="col-8 text-center m-5">
+                    <h5>
+                      Have new idea for cool project,but don’t have profeccional
+                      team to make it real.All you need is:
+                    </h5>
+                    <ol class='text-left'>
+                      <li>Fill our form</li>
+                      <li>Find out the total price</li>
+                      <li>
+                        Find out the total price Make an order to our
+                        proffecionals
+                      </li>
+                    </ol>
+                    <div class="w-50 pt-3 mx-auto">
+                    <a
+                      href="#"
+                      class="btn text-uppercase w-100 mx-auto d-none d-md-block"
+                      >log out</a
+                    >
+                  </div>
+                  </div>
+                </b-row>
+              </div>
+            </b-row>
           </b-container>
-
         </b-container>
       </b-container>
     </b-row>
@@ -60,3 +94,12 @@ export default {
   name: "projects",
 };
 </script>
+<style lang="scss">
+.anketa-box {
+  background-color: white;
+  border-radius: 40px;
+  box-shadow: 0 10px 22px rgba(0, 0, 0, 0.2);
+  -webkit-box-shadow: 0 10px 22px rgba(0, 0, 0, 0.2);
+  -moz-box-shadow: 0 10px 22px rgba(0, 0, 0, 0.2);
+}
+</style>
