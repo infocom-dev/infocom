405c406aab4466742b05dc039c7ebb868bc6fb43
TatyanaKonina <tdkonina@edu.hse.ru>
2021-05-27 13:48:03 +0300

    clean

--- src/api/auth.js
+++ src/api/auth.js
@@ -13,22 +13,4 @@ export default {
     
     return session.post('/registration/', {  password1, password2, email,phone });
   },
-  changeAccountPassword(password1, password2) {
-    return session.post('/auth/password/change/', { password1, password2 });
-  },
-  sendAccountPasswordResetEmail(email) {
-    return session.post('/auth/password/reset/', { email });
-  },
-  resetAccountPassword(uid, token, new_password1, new_password2) { // eslint-disable-line camelcase
-    return session.post('/auth/password/reset/confirm/', { uid, token, new_password1, new_password2 });
-  },
-  getAccountDetails() {
-    return session.get('/auth/user/');
-  },
-  updateAccountDetails(data) {
-    return session.patch('/auth/user/', data);
-  },
-  verifyAccountEmail(key) {
-    return session.post('/registration/verify-email/', { key });
-  },
 };
--- src/store/signup.js
+++ src/store/signup.js
@@ -1,10 +1,6 @@
 import auth from '../api/auth';
 
 import {
-  ACTIVATION_BEGIN,
-  ACTIVATION_CLEAR,
-  ACTIVATION_FAILURE,
-  ACTIVATION_SUCCESS,
   REGISTRATION_BEGIN,
   REGISTRATION_CLEAR,
   REGISTRATION_FAILURE,
@@ -14,9 +10,6 @@ import {
 export default {
   namespaced: true,
   state: {
-    activationCompleted: false,
-    activationError: false,
-    activationLoading: false,
     registrationCompleted: false,
     registrationError: false,
     registrationLoading: false,
@@ -30,37 +23,11 @@ export default {
         .then(() => commit(REGISTRATION_SUCCESS))
         .catch(() => commit(REGISTRATION_FAILURE));
     },
-    activateAccount({ commit }, { key }) {
-      commit(ACTIVATION_BEGIN);
-      return auth.verifyAccountEmail(key)
-        .then(() => commit(ACTIVATION_SUCCESS))
-        .catch(() => commit(ACTIVATION_FAILURE));
-    },
     clearRegistrationStatus({ commit }) {
       commit(REGISTRATION_CLEAR);
     },
-    clearActivationStatus({ commit }) {
-      commit(ACTIVATION_CLEAR);
-    },
   },
   mutations: {
-    [ACTIVATION_BEGIN](state) {
-      state.activationLoading = true;
-    },
-    [ACTIVATION_CLEAR](state) {
-      state.activationCompleted = false;
-      state.activationError = false;
-      state.activationLoading = false;
-    },
-    [ACTIVATION_FAILURE](state) {
-      state.activationError = true;
-      state.activationLoading = false;
-    },
-    [ACTIVATION_SUCCESS](state) {
-      state.activationCompleted = true;
-      state.activationError = false;
-      state.activationLoading = false;
-    },
     [REGISTRATION_BEGIN](state) {
       state.registrationLoading = true;
     },
